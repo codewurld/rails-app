d9063d879e6f728511e5799cc3b2f512aa8018a2
Ubuntu <ubuntu@ip-172-31-86-228.ec2.internal>
2022-05-30 12:30:38 +0000

    Initialise repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  def hello
+    render html: "¡hola, mundo!"
+  end 
+end
+
+class ApplicationController < ActionController::Base
+  def goodbye
+    render html: "goodbye, world!"
+  end
+end
